b7fe713e6ba21b722e10f9fdd64ca87e821b430d
github username <github email address>
2019-08-13 14:50:41 +0100

    Seven Seas Lab Done

--- Rails-Seven-Seas/app/controllers/seas_controller.rb
+++ Rails-Seven-Seas/app/controllers/seas_controller.rb
@@ -1,13 +1,49 @@
 class SeasController < ApplicationController
   #define your controller actions here
 
+  def welcome
+  end 
+
+  def index
+    @seas = Sea.all 
+  end 
+
+  def new 
+    @sea = Sea.new
+  end 
+
+  def show 
+    @sea = Sea.find(params[:id])
+  end 
+
+  def create 
+    @sea = Sea.create(sea_params)
+    redirect_to seas_path
+  end 
+
+  def edit
+    @sea = Sea.find(params[:id])
+  end 
+
+  def update 
+    @sea = Sea.find(params[:id])
+    @sea.update(sea_params) 
+    redirect_to sea_path(@sea)
+  end 
+
+  def destroy 
+    Sea.destroy(params[:id])
+    redirect_to seas_path
+  end 
 
   private
-  # In controller actions, use this private method to access sea params from forms.
-  # Example: @sea.update(sea_params)
-  # check the return value of sea_params to see what you're working with!
-  def sea_params
+   def sea_params
     params.require(:sea).permit(:name, :temperature, :bio, :mood, :image_url, :favorite_color, :scariest_creature, :has_mermaids)
   end
 
 end
+
+
+ # In controller actions, use this private method to access sea params from forms.
+  # Example: @sea.update(sea_params)
+  # check the return value of sea_params to see what you're working with!
--- Rails-Seven-Seas/config/routes.rb
+++ Rails-Seven-Seas/config/routes.rb
@@ -1,4 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+ get "/", to: "seas#welcome"
+
+ get "/seas", to: "seas#index", as: "seas"
+
+ get "/seas/new", to: "seas#new", as: "new_sea"
+
+ get "/seas/:id", to: "seas#show", as: "sea"
+
+ post "/seas", to: "seas#create"
+
+ get "/seas/:id/edit", to: "seas#edit", as: "edit_sea"
+
+ patch "/seas/:id", to: "seas#update"
+
+ delete "/seas/:id", to: "seas#destroy"
 
 end
